8c079dc2698b305215a3d108d15dba5d1bbff988
Scott Cox <scottcox89@gmail.com>
2017-05-09 00:42:13 +0100

    Adds VictoryChart and conditional render logic.

--- src/containers/Results.js
+++ src/containers/Results.js
@@ -1,14 +1,40 @@
 import React from 'react';
 import {connect} from 'react-redux';
 
-import { VictoryPie, VictoryBar, VictoryLabel } from 'victory';
+import {VictoryPie, VictoryBar, VictoryTheme, VictoryChart, VictoryAxis, VictoryLabel} from 'victory';
 
 let Results = ({data}) => {
-	return (
-		<div id="results">
-			<VictoryPie data={data} x="name" y="quantity" />
-		</div>
-	)
+	if (data.length > 0) {
+		return (
+			<div id="results">
+				<VictoryPie data={data}
+							x="name"
+							y="quantity"
+							theme={VictoryTheme.material}
+							width={600}
+							height={400}
+				/>
+				<VictoryChart theme={VictoryTheme.material}
+							  width={600}
+							  height={400}
+							  padding={{top: 50, right: 50, bottom: 150, left: 50}}
+				>
+					<VictoryAxis tickLabelComponent={<VictoryLabel angle={90} verticalAnchor="middle" textAnchor="start"/>} />
+					<VictoryBar data={data}
+								x="name"
+								y="quantity"
+								theme={VictoryTheme.material}
+								labels={(datum) => datum.quantity}
+								padding={20}
+					/>
+				</VictoryChart>
+			</div>
+		)
+	} else {
+		return (
+			<div id="results"></div>
+		)
+	}
 }
 
 const _mapStateToProps = (state) => ({
